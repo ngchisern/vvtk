fd0bc1c058bcf03bc8b33dfce0e0cd3061c6f6bb
ngchisern <ncsern2000@gmail.com>
2023-10-16 10:48:56 +0800

    Add midpoint module

---
+++ src/midpoint/generate.rs
@@ -0,0 +1,27 @@
+use crate::formats::{pointxyzrgba::PointXyzRgba, PointCloud};
+
+// bounding box of the point cloud
+pub fn generate_midpoint(points: PointCloud<PointXyzRgba>) -> [f32; 3] {
+    let first_point = points.points[0];
+    let mut min_x = first_point.x;
+    let mut max_x = first_point.x;
+    let mut min_y = first_point.y;
+    let mut max_y = first_point.y;
+    let mut min_z = first_point.z;
+    let mut max_z = first_point.z;
+
+    for &point in &points.points {
+        min_x = min_x.min(point.x);
+        max_x = max_x.max(point.x);
+        min_y = min_y.min(point.y);
+        max_y = max_y.max(point.y);
+        min_z = min_z.min(point.z);
+        max_z = max_z.max(point.z);
+    }
+
+    [
+        (max_x - min_x) / 2,
+        (max_y - min_y) / 2,
+        (max_z - min_z) / 2,
+    ]
+}
